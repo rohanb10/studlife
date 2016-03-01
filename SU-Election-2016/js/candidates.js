774e3cf9395abee71811e3b5c8ffe9d4f8c8129c
Rohan Bhansali <rohanb10@gmail.com>
2016-03-01 14:24:03 -0600

    Working dropdowns

---
+++ SU-Election-2016/js/candidates.js
@@ -0,0 +1,36 @@
+//p, vpa, vpf, vppr, vpp
+var candidates = [
+{
+	id:"vppr-1",
+	position:"Vice President of Public Relations",
+	name:"Yash Dalal",
+	class:"2017",
+	description: "I am the next VP",
+	img:""
+},
+{
+	id:"vppr-2",
+	position:"Vice President of Public Relations",
+	name:"Rohan B",
+	class:"2017",
+	description: "baseball474747@gmail.com",
+	img:""
+},
+{
+	id:"vpp-2",
+	position:"Vice President of Public Relations",
+	name:"Rohan B",
+	class:"2017",
+	description: "cdfbhjdbjh@gmail.com",
+	img:""
+},
+{
+	id:"vpp-1",
+	position:"Vice President of Public Relations",
+	name:"Rohan B",
+	class:"2016",
+	description: "somethingsomething@gmail.com",
+	img:""
+},
+
+]
