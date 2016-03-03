f045ed864d86a4e0fcbb593c4bd69e81198ced53
Rohan Bhansali <rohanb10@gmail.com>
2016-03-02 19:54:43 -0600

    added candidate basic info

--- SU-Election-2016/js/candidates.js
+++ SU-Election-2016/js/candidates.js
@@ -1,36 +1,91 @@
 //p, vpa, vpf, vppr, vpp
 var candidates = [
 {
-	id:"vppr-1",
-	position:"Vice President of Public Relations",
-	name:"Yash Dalal",
+	id:"p-1",
+	position:"President",
+	name:"Demetri Kavadas",
 	class:"2017",
 	description: "I am the next VP",
 	img:""
 },
 {
-	id:"vppr-2",
-	position:"Vice President of Public Relations",
-	name:"Rohan B",
+	id:"p-2",
+	position:"President",
+	name:"Kenneth Sng",
 	class:"2017",
-	description: "baseball474747@gmail.com",
+	description: "I am the next VP",
 	img:""
 },
 {
-	id:"vpp-2",
-	position:"Vice President of Public Relations",
-	name:"Yash Dalal",
+	id:"p-3",
+	position:"President",
+	name:"Mike Holtz",
 	class:"2017",
-	description: "cdfbhjdbjh@gmail.com",
+	description: "I am the next VP",
+	img:""
+},
+{
+	id:"vpa-1",
+	position:"Vice President of Administration",
+	name:"Chris Wilson",
+	class:"2017",
+	description: "I am the next VP",
+	img:""
+},
+{
+	id:"vpa-2",
+	position:"Vice President of Administration",
+	name:"Noah Silverman",
+	class:"2019",
+	description: "I am the next VP",
+	img:""
+},
+{
+	id:"vpa-3",
+	position:"Vice President of Administration",
+	name:"Sankalp Kapur",
+	class:"2018",
+	description: "I am the next VP",
+	img:""
+},
+{
+	id:"vpf-1",
+	position:"Vice President of Finance",
+	name:"Keaton Schifer",
+	class:"2017",
+	description: "I am the next VP",
+	img:""
+},
+{
+	id:"vpf-2",
+	position:"Vice President of Finance",
+	name:"Omar Abdelaziz",
+	class:"2017",
+	description: "I am the next VP",
+	img:""
+},
+{
+	id:"vpf-3",
+	position:"Vice President of Finance",
+	name:"Vikram Biswas",
+	class:"2017",
+	description: "I am the next VP",
+	img:""
+},
+{
+	id:"vppr-1",
+	position:"Vice President of Public Relations",
+	name:"Amelia Fong",
+	class:"2017",
+	description: "I am the next VP",
 	img:""
 },
 {
 	id:"vpp-1",
-	position:"Vice President of Public Relations",
-	name:"Rohan B",
-	class:"2016",
-	description: "somethingsomething@gmail.com",
+	position:"Vice President of Programming",
+	name:"Richard Wu",
+	class:"2017",
+	description: "I am the next VP",
 	img:""
 },
-
 ]
