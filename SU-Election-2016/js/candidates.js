08da1903c4c0db478c72470bf17976c78ae467a5
Rohan Bhansali <rohanb10@gmail.com>
2016-03-02 23:44:20 -0600

    Finished a lot

--- SU-Election-2016/js/candidates.js
+++ SU-Election-2016/js/candidates.js
@@ -4,88 +4,99 @@ var candidates = [
 	id:"p-1",
 	position:"President",
 	name:"Demetri Kavadas",
-	class:"2017",
-	description: "I am the next VP",
-	img:""
+	class:"2018",
+	description: "Hi. My name is Demetri Kavadas, though most of you may know me as “Ninja Turtle Backpack Guy” or some variation thereof. Having transferred from the University of Michigan to Washington University this fall, I immediately fell in love with the campus and the students, faculty and staff that inhabit it. I very much feel apart of the WashU family and can only express my thanks to each of you for the wonderful sense of community you have helped create here. This being said, I am asking for the opportunity to further my gratitude by serving you in the Student Union. While I admittedly have no prior student government experience, I consider myself a hard worker who puts effort and passion into everything I do. Moreover, I listen better than I talk and thereby promise to bring such receptivity to SU as I strive to represent your interests in their purest form. My goal is unspecific: make Washington University even greater than it already is. Together we can further augment the currently wonderful community at WashU. To quote John Lennon’s “Imagine” (a fantastic song which I would strongly recommend to all musical tastes), “You may say I'm a dreamer, but I'm not the only one. I hope someday you'll join us, and the world will be as one.” Thank You, and I look forward to seeing you around campus.",
+	img:"img/demetrikavadas.jpg",
+	document:"Demetri Kavadas_2018_ExecPresident.docx"
 },
 {
 	id:"p-2",
 	position:"President",
 	name:"Kenneth Sng",
 	class:"2017",
-	description: "I am the next VP",
-	img:""
+	description: "",
+	img:"img/kennethsng.jpg",
+	document:"Kenneth Sng_2017_ExecPresident.docx"
 },
 {
 	id:"p-3",
 	position:"President",
 	name:"Mike Holtz",
 	class:"2017",
-	description: "I am the next VP",
-	img:""
+	description: "",
+	img:"img/",
+	document:"Mike Holtz_2017_ExecPresident.docx"
 },
 {
 	id:"vpa-1",
 	position:"Vice President of Administration",
 	name:"Chris Wilson",
 	class:"2017",
-	description: "I am the next VP",
-	img:""
+	description: "",
+	img:"img/",
+	document:"Chris Wilson_2017_ExecVPAdmin.docx"
 },
 {
 	id:"vpa-2",
 	position:"Vice President of Administration",
 	name:"Noah Silverman",
 	class:"2019",
-	description: "I am the next VP",
-	img:""
+	description: "",
+	img:"img/",
+	document:"Noah Silverman_2019_ExecVPAdmin.docx"
 },
 {
 	id:"vpa-3",
 	position:"Vice President of Administration",
 	name:"Sankalp Kapur",
 	class:"2018",
-	description: "I am the next VP",
-	img:""
+	description: "This is a test",
+	img:"img/",
+	document:"Sankalp Kapur_2018_ExecVPAdmin.docx"
 },
 {
 	id:"vpf-1",
 	position:"Vice President of Finance",
 	name:"Keaton Schifer",
-	class:"2017",
-	description: "I am the next VP",
-	img:""
+	class:"2018",
+	description: "",
+	img:"img/",
+	document:"Keaton Schifer_2018_ExecVPFinance.docx"
 },
 {
 	id:"vpf-2",
 	position:"Vice President of Finance",
 	name:"Omar Abdelaziz",
-	class:"2017",
-	description: "I am the next VP",
-	img:""
+	class:"2018",
+	description: "",
+	img:"img/",
+	document:"Omar Abdelaziz_2018_ExecVPFinance.docx"
 },
 {
 	id:"vpf-3",
 	position:"Vice President of Finance",
 	name:"Vikram Biswas",
 	class:"2017",
-	description: "I am the next VP",
-	img:""
+	description: "",
+	img:"img/",
+	document:"Vikram Biswas_2017_ExecVPFinance.docx"
 },
 {
 	id:"vppr-1",
 	position:"Vice President of Public Relations",
 	name:"Amelia Fong",
-	class:"2017",
-	description: "I am the next VP",
-	img:""
+	class:"2018",
+	description: "",
+	img:"img/",
+	document:"Amelia Fong_2018_ExecVPPR.docx"
 },
 {
 	id:"vpp-1",
 	position:"Vice President of Programming",
 	name:"Richard Wu",
-	class:"2017",
-	description: "I am the next VP",
-	img:""
+	class:"2018",
+	description: "",
+	img:"img/",
+	document:"Richard Wu _2018_ExecVPProgramming.docx"
 },
 ]
