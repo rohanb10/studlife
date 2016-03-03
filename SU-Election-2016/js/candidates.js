673b6d7fc37d8507c0fc6d3dfb24eb6f0ab488ee
Rohan Bhansali <rohanb10@gmail.com>
2016-03-03 00:55:34 -0600

    not a

--- SU-Election-2016/js/candidates.js
+++ SU-Election-2016/js/candidates.js
@@ -1,11 +1,10 @@
-//p, vpa, vpf, vppr, vpp
 var candidates = [
 {
 	id:"p-1",
 	position:"President",
 	name:"Demetri Kavadas",
 	class:"2018",
-	description: "Hi. My name is Demetri Kavadas, though most of you may know me as “Ninja Turtle Backpack Guy” or some variation thereof. Having transferred from the University of Michigan to Washington University this fall, I immediately fell in love with the campus and the students, faculty and staff that inhabit it. I very much feel apart of the WashU family and can only express my thanks to each of you for the wonderful sense of community you have helped create here. This being said, I am asking for the opportunity to further my gratitude by serving you in the Student Union. While I admittedly have no prior student government experience, I consider myself a hard worker who puts effort and passion into everything I do. Moreover, I listen better than I talk and thereby promise to bring such receptivity to SU as I strive to represent your interests in their purest form. My goal is unspecific: make Washington University even greater than it already is. Together we can further augment the currently wonderful community at WashU. To quote John Lennon’s “Imagine” (a fantastic song which I would strongly recommend to all musical tastes), “You may say I'm a dreamer, but I'm not the only one. I hope someday you'll join us, and the world will be as one.” Thank You, and I look forward to seeing you around campus.",
+	description: "My goal is unspecific: make Washington University even greater than it already is. Together we can further augment the currently wonderful community at WashU. To quote John Lennon’s “Imagine” (a fantastic song which I would strongly recommend to all musical tastes), <em>You may say I'm a dreamer, but I'm not the only one. I hope someday you'll join us, and the world will be as one.</em>",
 	img:"img/demetrikavadas.jpg",
 	document:"Demetri Kavadas_2018_ExecPresident.docx"
 },
@@ -14,7 +13,7 @@ var candidates = [
 	position:"President",
 	name:"Kenneth Sng",
 	class:"2017",
-	description: "",
+	description: "As SU President, I intend to create a student-first Student Union. I am committed to listening to and engaging with students. Major decisions will not be made without extensive student consultation. Together, we will bring undergraduate programming to greater heights. We will also expand on our advocacy efforts to address issues students care about such as inclusiveness, mental health, sexual violence, and socioeconomic diversity.",
 	img:"img/kennethsng.jpg",
 	document:"Kenneth Sng_2017_ExecPresident.docx"
 },
@@ -23,7 +22,7 @@ var candidates = [
 	position:"President",
 	name:"Mike Holtz",
 	class:"2017",
-	description: "",
+	description: "TL;DR: My name is Mike. I have a lot of leadership experience in SU. I truly believe SU must be more than just a budget, and that we need to put a focus on advocacy. If we work together, we can make SU into an honest advocate for students—one that brings the Wash. U. community together to create an unforgettable college experience.",
 	img:"img/",
 	document:"Mike Holtz_2017_ExecPresident.docx"
 },
@@ -32,7 +31,7 @@ var candidates = [
 	position:"Vice President of Administration",
 	name:"Chris Wilson",
 	class:"2017",
-	description: "",
+	description: "One major goal of mine over the course of the following year is to allow students’ opinions to be voiced and taken into account before making major decisions. We can do this by increasing transparency in the decision making process and also shifting Student Union to a more proactive rather than reactive body.",
 	img:"img/",
 	document:"Chris Wilson_2017_ExecVPAdmin.docx"
 },
@@ -41,7 +40,7 @@ var candidates = [
 	position:"Vice President of Administration",
 	name:"Noah Silverman",
 	class:"2019",
-	description: "",
+	description: "Student Union executives are often faced with challenging decisions that can have unpopular ramifications. I am prepared for this, and will ensure that Student Union maintains the highest standards for transparency during the decision-making process. One of the ways I will achieve this is by livestreaming and videotaping our Senate, Treasury, and General Budget Meetings. I also propose a public Student Union calendar that will enable students to know when and where Student Union meetings take place, so you can express your views. Finally, I will work with the next Executive Council to ensure that the student voice is given appropriate time and space during other meetings. One of the ways I will achieve [transparency] is by livestreaming and videotaping our Senate, Treasury, and General Budget Meetings. I also propose a public Student Union calendar that will enable students to know when and where Student Union meetings take place, so you can express your views. Finally, I will work with the next Executive Council to ensure that the student voice is given appropriate time and space during other meetings.",
 	img:"img/",
 	document:"Noah Silverman_2019_ExecVPAdmin.docx"
 },
@@ -50,7 +49,7 @@ var candidates = [
 	position:"Vice President of Administration",
 	name:"Sankalp Kapur",
 	class:"2018",
-	description: "This is a test",
+	description: "",
 	img:"img/",
 	document:"Sankalp Kapur_2018_ExecVPAdmin.docx"
 },
