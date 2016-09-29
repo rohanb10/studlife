fe8f8c3fad039eda75fd81106ec4d08e76539fa0
Rohan Bhansali <rohanb10@gmail.com>
2016-09-29 13:34:47 -0500

    IC of debates. Only includes initial layout. Will connect to WP database, on next commit

---
+++ debate/page-debate.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Template Name: Debate
+*/
+?>
+
+<?php get_header();
+//Breaking out of site-container
+?>
+</div>
+
+<style type="text/css">
+</style>
+
+<div class="static-house">
+		<h1 class="tagline">Student Life | Debate 2016</h1>
+</div>
+
+<div class="site-container">
+	<?php get_footer(); ?>
+</div>
